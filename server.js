829c8612110ea797fc6206f0c350867a9fd336ff
Anton Piniaz <antonsgrouva@gmail.com>
2020-03-05 14:09:55 +0200

    Removed BC auth

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ app.all('*', (req, res, next) => {
     // Set CORS headers: allow all origins, methods, and headers
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Auth-Token, X-Auth-Client, Target-URL');
+    res.set('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Target-URL');
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.status(200).send();
     } else {
         const targetURL = req.get('Target-URL');
-        const authClient = req.get('X-Auth-Client');
-        const authToken = req.get('X-Auth-Token');
         if (!targetURL) {
             res.status(500).send(
                 'There is no Target-URL header in the request'
@@ -27,10 +25,6 @@ app.all('*', (req, res, next) => {
                 url: targetURL,
                 method: req.method,
                 json: req.body,
-                headers: {
-                    'X-Auth-Client': authClient,
-                    'X-Auth-Token': authToken
-                }
             },
             (error, response, body) => {
                 if (error) {
